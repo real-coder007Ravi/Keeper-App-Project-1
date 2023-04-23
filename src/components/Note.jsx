2efdcc6461354d464e924adaf0649488fd651db6
Ravi Kumar Gupta <87241044+real-coder007Ravi@users.noreply.github.com>
2023-04-23 13:46:46 +0530

    Add files via upload

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1>This is the Heading</h1>
+      <p>This is the Description</p>
+    </div>
+  );
+}
+export default Note;
